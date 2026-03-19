5270bd93ea76f3ae76442e8d6fcc2fea2c0bca45
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-03-18 19:01:37 -0700

    restructure

--- ttt.js
+++ ttt.js
@@ -8,39 +8,41 @@ let tttBoard = {
     { name: "playerA", symbol: "" },
     { name: "playerB", symbol: "" },
   ],
-
-  tttLogicController() {
-    const getPlayerSymbol = function (playerName) {
-      const player = this.playerData.find((p) => p.name === playerName);
-      return prompt(`${player.name}:`);
-    }.bind(this);
-
-    const setPlayerSymbol = function (playerName) {
-      const player = this.playerData.find((p) => p.name === playerName);
-      return (player.symbol = getPlayerSymbol(playerName));
-    }.bind(this);
-
-    const setInitialPlayerState = function () {
-      this.playerData[0].symbol = setPlayerSymbol(this.playerData[0].name);
-      this.playerData[1].symbol = setPlayerSymbol(this.playerData[1].name);
-    }.bind(this);
-
-    return {
-      getPlayerSymbol: getPlayerSymbol,
-      setPlayerSymbol: setPlayerSymbol,
-      setInitialPlayerState: setInitialPlayerState,
-    };
-  },
-
-  tttDisplayController() {
-    const printConsole = function () {
-      console.table(this.playerData);
-      console.table(this.boardGrid);
-    }.bind(this);
-
-    return { printConsole: printConsole };
-  },
 };
 
-tttBoard.tttLogicController().setInitialPlayerState();
-tttBoard.tttDisplayController().printConsole();
+const tttLogic = (function logicController() {
+  const getPlayerSymbol = function (playerName) {
+    const player = tttBoard.playerData.find((p) => p.name === playerName);
+    return prompt(`${player.name}:`);
+  };
+
+  const setPlayerSymbol = function (playerName) {
+    const player = tttBoard.playerData.find((p) => p.name === playerName);
+    return (player.symbol = getPlayerSymbol(playerName));
+  };
+
+  const setInitialPlayerState = function () {
+    tttBoard.playerData[0].symbol = setPlayerSymbol(tttBoard.playerData[0].name);
+    tttBoard.playerData[1].symbol = setPlayerSymbol(tttBoard.playerData[1].name);
+  };
+
+  return {
+    getPlayerSymbol: getPlayerSymbol,
+    setPlayerSymbol: setPlayerSymbol,
+    setInitialPlayerState: setInitialPlayerState,
+  };
+})();
+
+const tttDisplay = (function displayController() {
+  const printConsole = function () {
+    console.table(tttBoard.playerData);
+    console.table(tttBoard.boardGrid);
+  };
+
+  return { printConsole: printConsole };
+})();
+
+(function tttMain() {
+  tttLogic.setInitialPlayerState();
+  tttDisplay.printConsole();
+})();
