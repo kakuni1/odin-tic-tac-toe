de82f20ddda58f1048825214f54c5152329aac90
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-03-18 17:22:57 -0700

    reorganize: logic & display

--- ttt.js
+++ ttt.js
@@ -4,8 +4,43 @@ let tttBoard = {
     .fill()
     .map(() => Array(3).fill("")),
 
-  playerA: { name: "A" },
-  playerB: { name: "B" },
+  playerData: [
+    { name: "playerA", symbol: "" },
+    { name: "playerB", symbol: "" },
+  ],
+
+  tttLogicController() {
+    const getPlayerSymbol = function (playerName) {
+      const player = this.playerData.find((p) => p.name === playerName);
+      return prompt(`${player.name}:`);
+    }.bind(this);
+
+    const setPlayerSymbol = function (playerName) {
+      const player = this.playerData.find((p) => p.name === playerName);
+      return (player.symbol = getPlayerSymbol(playerName));
+    }.bind(this);
+
+    const setInitialPlayerState = function () {
+      this.playerData[0].symbol = setPlayerSymbol(this.playerData[0].name);
+      this.playerData[1].symbol = setPlayerSymbol(this.playerData[1].name);
+    }.bind(this);
+
+    return {
+      getPlayerSymbol: getPlayerSymbol,
+      setPlayerSymbol: setPlayerSymbol,
+      setInitialPlayerState: setInitialPlayerState,
+    };
+  },
+
+  tttDisplayController() {
+    const printConsole = function () {
+      console.table(this.playerData);
+      console.table(this.boardGrid);
+    }.bind(this);
+
+    return { printConsole: printConsole };
+  },
 };
 
-console.log(tttBoard);
+tttBoard.tttLogicController().setInitialPlayerState();
+tttBoard.tttDisplayController().printConsole();
