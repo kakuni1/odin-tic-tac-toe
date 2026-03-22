6d12438829a911cd1d3fe782342dd525fc715906
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-03-21 17:03:18 -0700

    rearrange

--- ttt.js
+++ ttt.js
@@ -4,45 +4,43 @@ let tttBoard = {
     .fill("")
     .map(() => Array(3).fill("")),
 
-  playerOne: { name: "Player 1" },
-  playerTwo: { name: "Player 2" },
+  playerA: { name: "", symbol: "" },
+  playerB: { name: "", symbol: "" },
 };
 
 const prompt = require("prompt-sync")();
 
 (function runLogic() {
-  let firstPlayer, secondPlayer, firstSymbol, secondSymbol;
-
-  function runInitialSetup() {
-    // randomly pick first player, first player symbol: x, second: o
+  (function runInitialSetup() {
     const coinFlip = Math.random();
-    if (coinFlip > 0.5) {
-      // playerA, first
-      firstPlayer = "playerA";
-      secondPlayer = "playerB";
-      firstSymbol = "x";
-      secondSymbol = "o";
-    } else {
-      // playerB, first
-      firstPlayer = "playerB";
-      secondPlayer = "playerA";
-      firstSymbol = "o";
-      secondSymbol = "x";
-    }
-  }
 
-  function promptUser(player, symbol) {
-    const inputRow = prompt(`${player} (${symbol}), choose array slot (row): `);
-    const inputCol = prompt(`${player} (${symbol}), choose array slot (col): `);
+    // randomly apply symbol, "x" & "o"
+    // x, goes first
+    if (coinFlip > 0.5) {
+      tttBoard.playerA.symbol = "x";
+      tttBoard.playerB.symbol = "o";
+      tttBoard.playerA.name = "Player 1";
+      tttBoard.playerB.name = "Player 2";
+    } else {
+      tttBoard.playerB.symbol = "x";
+      tttBoard.playerA.symbol = "o";
+      tttBoard.playerB.name = "Player 1";
+      tttBoard.playerA.name = "Player 2";
+    }
+  })();
+
+  function promptUser() {
+    const inputRow = prompt(`${tttBoard.playerA.name} (${tttBoard.playerA.symbol}), choose array slot (row): `);
+    const inputCol = prompt(`${tttBoard.playerB.name} (${tttBoard.playerB.symbol}), choose array slot (col): `);
     return { row: parseInt(inputRow) - 1, col: parseInt(inputCol) - 1 };
   }
 
-  function oneMove(player, symbol) {
+  function oneMove(symbol) {
     let validMove = false;
     // take (1) player's input
 
     while (!validMove) {
-      const { row, col } = promptUser(player, symbol);
+      const { row, col } = promptUser();
       if (tttBoard.boardGrid[row][col] === "") {
         validMove = true;
         tttBoard.boardGrid[row][col] = symbol;
@@ -50,28 +48,28 @@ const prompt = require("prompt-sync")();
     }
   }
 
-  function runGame(firstPlayer, secondPlayer, firstSymbol, secondSymbol) {
+  function runGame() {
     // loop game until exit condition is met
     while (true) {
       if (exitNow("x") === true) {
-        console.log(`WINNER: ${tttBoard.playerOne.name}`);
+        console.log(`WINNER: ${tttBoard.playerA.name}`);
         break;
       }
       if (exitNow("o") === true) {
-        console.log(`WINNER: ${tttBoard.playerTwo.name}`);
+        console.log(`WINNER: ${tttBoard.playerB.name}`);
         break;
       }
-      oneMove(firstPlayer, firstSymbol);
+      oneMove("x");
       printConsole();
       if (exitNow("x") === true) {
-        console.log(`WINNER: ${tttBoard.playerOne.name}`);
+        console.log(`WINNER: ${tttBoard.playerA.name}`);
         break;
       }
       if (exitNow("o") === true) {
-        console.log(`WINNER: ${tttBoard.playerTwo.name}`);
+        console.log(`WINNER: ${tttBoard.playerB.name}`);
         break;
       }
-      oneMove(secondPlayer, secondSymbol);
+      oneMove("o");
       printConsole();
     }
   }
@@ -82,7 +80,10 @@ const prompt = require("prompt-sync")();
     // check for exit conditions, return true to exit
 
     // board full
-    if (board.some((i) => i.includes("")) === false) return true;
+    if (board.some((i) => i.includes("")) === false) {
+      console.log("TIE");
+      return true;
+    }
     // row check
     if (board[0][0] === symbol && board[0][1] === symbol && board[0][2] === symbol) return true;
     if (board[1][0] === symbol && board[1][1] === symbol && board[1][2] === symbol) return true;
@@ -100,20 +101,24 @@ const prompt = require("prompt-sync")();
   }
 
   function nameChange() {
-    let whichName = prompt(`Select name -> [1] ${tttBoard.playerOne.name}, [2] ${tttBoard.playerTwo.name}:  `);
+    let whichName = prompt(`Select name -> [1] ${tttBoard.playerA.name}, [2] ${tttBoard.playerA.name}:  `);
     whichName = parseInt(whichName);
     const inputName = prompt(`Enter name: `);
-    if (whichName === 1) tttBoard.playerOne.name = inputName;
-    if (whichName === 2) tttBoard.playerTwo.name = inputName;
+    if (whichName === 1) tttBoard.playerA.name = inputName;
+    if (whichName === 2) tttBoard.playerB.name = inputName;
   }
 
   function printConsole() {
+    console.table(tttBoard.playerA);
+    console.table(tttBoard.playerB);
+    console.table(tttBoard.firstMove);
     console.table(tttBoard.boardGrid);
   }
 
   // run game (main)
-  runInitialSetup();
-  runGame(firstPlayer, secondPlayer, firstSymbol, secondSymbol);
+  // runInitialSetup();
+  runGame();
+  printConsole();
   (function endGame() {
     console.log("GAME END");
   })();
