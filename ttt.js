6c1c2202918aa0a8ea74691bf9058be19ac6735b
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-03-24 16:57:53 -0700

    major change: use click input instead of prompt

--- ttt.js
+++ ttt.js
@@ -1,15 +1,23 @@
-let tttBoard = {
-  // create empty "" 3x3 grid using arrays
-  boardGrid: Array(3)
-    .fill("")
-    .map(() => Array(3).fill("")),
-
-  playerA: { name: "", symbol: "" },
-  playerB: { name: "", symbol: "" },
-  gameData: { winner: "", tie: false },
-};
-
 (function runLogic() {
+  let tttBoard = {
+    // create empty "" 3x3 grid using arrays
+    boardGrid: Array(3)
+      .fill("")
+      .map(() => Array(3).fill("")),
+
+    playerA: { name: "", symbol: "" },
+    playerB: { name: "", symbol: "" },
+    gameData: {
+      row: 0,
+      col: 0,
+      currentIndex: "",
+      currentSymbol: "x",
+      display: "Next: ",
+      winner: "",
+      tie: false,
+    },
+  };
+
   (function runInitialSetup() {
     const coinFlip = Math.random();
 
@@ -28,71 +36,136 @@ let tttBoard = {
     }
   })();
 
-  function getPlayer(symbol) {
-    return tttBoard.playerA.symbol === symbol ? tttBoard.playerA : tttBoard.playerB;
+  function readyGame() {
+    printPlayerInfo();
+    document.querySelectorAll(".card").forEach((card, index) => {
+      card.addEventListener("click", () => {
+        tttBoard.gameData.currentIndex = index;
+        tttBoard.gameData.row = Math.floor(index / 3);
+        tttBoard.gameData.col = index % 3;
+        checkMove();
+      });
+    });
   }
 
-  function runGame() {
-    // loop game until exit condition is met
-    while (true) {
-      oneMove("x");
-      if (exitNow("x") === true) {
-        tttBoard.gameData.winner = getPlayer("x").name;
-        printConsole();
-        break;
+  function checkMove() {
+    const row = tttBoard.gameData.row;
+    const col = tttBoard.gameData.col;
+    const index = tttBoard.gameData.currentIndex;
+    const symbol = tttBoard.gameData.currentSymbol;
+    const card = document.querySelectorAll(".card");
+    const currentCard = card[index];
+    const currentText = currentCard.querySelector(".display");
+
+    if (tttBoard.boardGrid[row][col] === "") {
+      tttBoard.boardGrid[row][col] = symbol;
+      currentText.textContent = symbol;
+
+      const gameOver = checkExit(symbol);
+
+      if (gameOver) {
+        endGame();
+        if (tttBoard.gameData.winner) {
+          tttBoard.gameData.display = "winner: " + tttBoard.gameData.winner;
+          tttBoard.gameData.currentSymbol = "";
+          printPlayerInfo();
+        } else {
+          tttBoard.gameData.display = "tie";
+          tttBoard.gameData.currentSymbol = "";
+          printPlayerInfo();
+        }
+        return;
       }
-      oneMove("o");
-      if (exitNow("o") === true) {
-        tttBoard.gameData.winner = getPlayer("o").name;
-        printConsole();
-        break;
-      }
+
+      switchSymbol();
+      printPlayerInfo();
+    } else {
+      currentText.style.color = "#B5514A";
+      currentCard.style.borderColor = "#B5514A";
+      currentCard.style.borderWidth = "0.225rem";
+      setTimeout(() => {
+        currentText.style.color = "";
+        currentCard.style.borderWidth = "";
+      }, 500);
     }
   }
 
-  function oneMove(symbol) {
-    let validMove = false;
-    // take (1) player's input
-
-    while (!validMove) {
-      const { row, col } = promptUser(symbol);
-      if (tttBoard.boardGrid[row][col] === "") {
-        validMove = true;
-        tttBoard.boardGrid[row][col] = symbol;
-        printConsole();
-      }
-    }
-  }
-
-  function exitNow(symbol) {
+  function checkExit(symbol) {
     const board = tttBoard.boardGrid;
 
     // check for exit conditions, return true to exit
+
+    if (
+      // row check
+      (board[0][0] === symbol &&
+        board[0][1] === symbol &&
+        board[0][2] === symbol) ||
+      (board[1][0] === symbol &&
+        board[1][1] === symbol &&
+        board[1][2] === symbol) ||
+      (board[2][0] === symbol &&
+        board[2][1] === symbol &&
+        board[2][2] === symbol) ||
+      // column check
+      (board[0][0] === symbol &&
+        board[1][0] === symbol &&
+        board[2][0] === symbol) ||
+      (board[0][1] === symbol &&
+        board[1][1] === symbol &&
+        board[2][1] === symbol) ||
+      (board[0][2] === symbol &&
+        board[1][2] === symbol &&
+        board[2][2] === symbol) ||
+      // diagonal check
+      (board[0][0] === symbol &&
+        board[1][1] === symbol &&
+        board[2][2] === symbol) ||
+      (board[0][2] === symbol &&
+        board[1][1] === symbol &&
+        board[2][0] === symbol)
+    ) {
+      tttBoard.gameData.winner = getPlayer(symbol).name;
+      return true;
+    }
 
     // board full
     if (board.some((i) => i.includes("")) === false) {
       tttBoard.gameData.tie = true;
       return true;
     }
-    // row check
-    if (board[0][0] === symbol && board[0][1] === symbol && board[0][2] === symbol) return true;
-    if (board[1][0] === symbol && board[1][1] === symbol && board[1][2] === symbol) return true;
-    if (board[2][0] === symbol && board[2][1] === symbol && board[2][2] === symbol) return true;
-    // column check
-    if (board[0][0] === symbol && board[1][0] === symbol && board[2][0] === symbol) return true;
-    if (board[0][1] === symbol && board[1][1] === symbol && board[2][1] === symbol) return true;
-    if (board[0][2] === symbol && board[1][2] === symbol && board[2][2] === symbol) return true;
-    // diagonal check
-    if (board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol) return true;
-    if (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol) return true;
 
     // if no exit condition, return false to resume
     return false;
   }
 
-  (function main() {
+  function switchSymbol() {
+    if (tttBoard.gameData.currentSymbol === "x") {
+      tttBoard.gameData.currentSymbol = "o";
+    } else {
+      tttBoard.gameData.currentSymbol = "x";
+    }
+  }
+
+  function printPlayerInfo() {
     document.getElementById("player-status").textContent =
-      `${tttBoard.playerA.name}:  ${tttBoard.playerA.symbol}\n${tttBoard.playerB.name}:  ${tttBoard.playerB.symbol}`;
-    runGame();
+      `${tttBoard.playerA.name}: ${tttBoard.playerA.symbol}\n${tttBoard.playerB.name}: ${tttBoard.playerB.symbol}
+      \n${tttBoard.gameData.display}${tttBoard.gameData.currentSymbol}`;
+    console.table(tttBoard.gameData);
+  }
+
+  function getPlayer(symbol) {
+    return tttBoard.playerA.symbol === symbol
+      ? tttBoard.playerA
+      : tttBoard.playerB;
+  }
+
+  function endGame() {
+    document.querySelectorAll(".card").forEach((card) => {
+      card.style.pointerEvents = "none";
+    });
+  }
+
+  (function main() {
+    readyGame();
   })();
 })();
